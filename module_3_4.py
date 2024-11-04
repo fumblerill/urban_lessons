3b03630c377cc7c9bedb7b7a9e419ecaf2373931
fumblerill <shidman01@yandex.ru>
2024-11-04 18:54:27 +1000

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -2,7 +2,6 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in range(len(other_words)):
-        #print(other_words[i].lower())
         if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
             same_words.append(other_words[i])
     return same_words
