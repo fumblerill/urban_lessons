8b3bf05770f333fde312eb492e064403b8981d54
fumblerill <shidman01@yandex.ru>
2024-11-10 19:02:08 +1000

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,9 +1,8 @@
 from fake_math import divide as df
 from true_math import divide as dt
 
-numbers=[]
-numbers.extend(input('Введите числа, в виде x, y: '))
-numbers = [int(char) for char in numbers if char.isdigit()]
+numbers = input('Введите числа, в виде x, y: ')
+numbers = [int(num) for num in numbers.replace(',', ' ').split() if num.isdigit()]
 
 print(f'Фейковая математика говорит {df(*numbers)}')
 print(f'Настоящая математика говорит {dt(*numbers)}')
