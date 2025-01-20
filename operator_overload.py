e9962915d1e697f1db2e33d5146832830aba7e1f
fumblerill <128781579+fumblerill@users.noreply.github.com>
2025-01-20 11:03:41 +1000

    Add files via upload

---
+++ operator_overload.py
@@ -0,0 +1,95 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __len__(self):
+        return  self.number_of_floors
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors == other.number_of_floors:
+                return True
+            else:
+                return False
+        raise TypeError('Ошибка')
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors < other.number_of_floors:
+                return True
+            else:
+                return False
+        raise TypeError('Ошибка')
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors <= other.number_of_floors:
+                return True
+            else:
+                return False
+        raise TypeError('Ошибка')
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors > other.number_of_floors:
+                return True
+            else:
+                return False
+        raise TypeError('Ошибка')
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors >= other.number_of_floors:
+                return True
+            else:
+                return False
+        raise TypeError('Ошибка')
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            if self.number_of_floors != other.number_of_floors:
+                return True
+            else:
+                return False
+        raise TypeError('Ошибка')
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            return House(self.name, self.number_of_floors + other)
+        raise TypeError('Ошибка')
+
+    def __iadd__(self, other):
+            return House(self.name, self.number_of_floors + other)
+
+    def __radd__(self, other):
+            return self.__add__(other)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+# __add__
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 #__radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
